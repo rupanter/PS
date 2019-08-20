e60d6a3cd88c083425921ccca75b6442eb7b20a8
Rupanter Chhabra <rupanterchhabra@Rupanters-MBP.guest.corp.microsoft.com>
2019-08-20 15:25:11 +0530

    Uploading test ps

---
+++ RemediatePolicy.ps1
@@ -0,0 +1,23 @@
+$connectionName = "AzureRunAsConnection"
+try {
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection) {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    }
+    else {
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+Start-AzPolicyRemediation -Name 'AzurePolicyRemediation' -PolicyAssignmentId '/subscriptions/f28f2c24-b393-4dda-b471-0ef3b69c0578/providers/Microsoft.Authorization/policyAssignments/AME-MMAInstallation-WindowsVMs-Assignment'
